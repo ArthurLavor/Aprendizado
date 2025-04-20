1568db3899589864b099ee443eacc90ffa24f7cc
Arthur De Lavor <arthurdelavor100@gmail.com>
2025-04-19 23:49:06 -0300

    feat: simple https server api

---
+++ server.js
@@ -0,0 +1,67 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const connection = require('./db'); 
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+ 
+  if (req.method === 'GET' && parsedUrl.pathname === '/') {
+    fs.readFile('index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        return res.end("Error loading the page");
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  
+ 
+  } else if (req.method === 'GET' && parsedUrl.pathname === '/tasks') {
+    connection.query('SELECT * FROM tasks', (err, results) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        return res.end(JSON.stringify({ error: 'Database error' }));
+      }
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(results));
+    });
+  
+ 
+  } else if (req.method === 'POST' && parsedUrl.pathname === '/tasks') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      try {
+        const task = JSON.parse(body);
+        connection.query(
+          'INSERT INTO tasks (title, description) VALUES (?, ?)',
+          [task.title, task.description],
+          (err, result) => {
+            if (err) {
+              res.writeHead(500, { 'Content-Type': 'application/json' });
+              return res.end(JSON.stringify({ error: 'Database error inserting task' }));
+            }
+            res.writeHead(201, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ id: result.insertId, title: task.title, description: task.description }));
+          }
+        );
+      } catch (error) {
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Invalid JSON' }));
+      }
+    });
+  
+ 
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end("Not Found");
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
